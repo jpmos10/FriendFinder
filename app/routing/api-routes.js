ff880ab93601acf2ba3f703f45930c094d346f76
Jason Mosley <jpmos10@users.noreply.github.com>
2016-11-08 16:58:07 -0600

    small modifications to files

---
+++ app/routing/api-routes.js
@@ -0,0 +1,10 @@
+var friendData = require('../data/friends.js');
+var path = require('path');
+
+
+
+module.exports = function(app){
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendData);
+	});
